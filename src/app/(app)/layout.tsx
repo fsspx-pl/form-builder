564ae5878c8f57c2d6c5188f40891bedc0340eba
Jacek Adamczyk <jacek@adamczyk.me>
2024-10-04 22:35:25 +0200

    payload local api

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,46 +1,44 @@
-import { ModalContainer, ModalProvider } from '@faceless-ui/modal'
-import React from 'react'
+import { ModalContainer, ModalProvider } from '@faceless-ui/modal';
+import payload from 'payload';
+import React from 'react';
 
-import type { MainMenu } from '../../payload-types'
-
-import { CloseModalOnRouteChange } from '../../components/CloseModalOnRouteChange'
-import { Header } from '../../components/Header'
-import '../../css/app.scss'
-import { GlobalsProvider } from '../../providers/Globals'
-
-export interface IGlobals {
-  mainMenu: MainMenu
-}
+import { MainMenu } from '@/payload-types';
+import { CloseModalOnRouteChange } from '../../components/CloseModalOnRouteChange';
+import { Header } from '../../components/Header';
+import '../../css/app.scss';
+import { GlobalsProvider } from '../../providers/Globals';
+import { getPayloadHMR } from '@payloadcms/next/utilities';
+import config from '@payload-config'
 
 export const metadata = {
   description: 'An example of how to authenticate with Payload from a Next.js app.',
   title: 'Payload Auth + Next.js App Router Example',
 }
 
-export const getAllGlobals = async (): Promise<IGlobals> => {
+async function getAllGlobals(): Promise<MainMenu> {
   try {
-    const [mainMenu] = await Promise.all([
-      fetch(`${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/globals/main-menu?depth=1`).then((res) => {
-        console.log(res);
-        return res.json()
-      }),
-    ])
-    return { mainMenu }
+    const payload = await getPayloadHMR({ config })
+    const mainMenu = await payload.findGlobal({
+      slug: 'main-menu',
+      depth: 1,
+    });
+
+    return mainMenu as MainMenu
   } catch (err) {
-    console.error('Error in getAllGlobals:', err)
-    throw new Error()
+    console.error('HELLO MOTO');
+    console.error('Error in getAllGlobals:', err);
+    throw new Error('Failed to fetch global data');
   }
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-  const globals = await getAllGlobals()
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const mainMenu = await getAllGlobals();
 
   return (
     <html lang="en">
       <body>
         <React.Fragment>
-          <GlobalsProvider {...globals}>
+          <GlobalsProvider mainMenu={mainMenu}>
             <ModalProvider classPrefix="form" transTime={0} zIndex="var(--modal-z-index)">
               <CloseModalOnRouteChange />
               <Header />
